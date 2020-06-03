1352bcb5ebf9389c252c0970a9f5c3b271042339
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-03 07:04:16 -0500

    styled home

--- avengers_phonebook/routes.py
+++ avengers_phonebook/routes.py
@@ -11,11 +11,14 @@ def home():
 def phonebook():
     # names = [('Captain America': '111-111-1111'), ('Iron Man': '222-222-2222'), ('Hulk': '333-333-3333'), ('Thor': '444-444-4444'), ('Black Widow': '555-555-5555'), ('Ms. Marvel': '666-666-6666'), ('Black Panthern': '777-777-7777'), ('Ant-Man': '888-888-8888'), ('Doctor Strange': '999-999-9999')]
     names = {
+        'Captain Marvel:': 18001234455,
         'Captain America:': 5555555555,
         'Iron Man:': 6302227878,
         'Hulk:': 9990001111,
+        'Black Widow:': 2304448899,
         'Thor:': 7792301718,
-        'Black Panthern:': 1111111111
+        'Black Panthern:': 1111111111,
+        'Ant Man:': 9999999999
     }
     return render_template('phonebook.html', names = names)
 
