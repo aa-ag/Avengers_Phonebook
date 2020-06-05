d3b0057686469606fcc630e12951f377e2ecd205
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-04 23:05:14 -0500

    v1.4

--- avengers_phonebook/routes.py
+++ avengers_phonebook/routes.py
@@ -13,6 +13,58 @@ def phonebook():
     nums = AvengerNum.query.all()
     return render_template('phonebook.html', nums = nums)
 
+@app.route('/submitnum', methods=['GET','POST'])
+@login_required
+def submitnum():
+    num = PostNum()
+    if request.method == 'POST' and num.validate():
+        name = num.avenger_name.data
+        phone = num.phone_num.data
+        user_id = current_user.id
+        print('\n', name, phone)
+        num = AvengerNum(name, phone, user_id)
+
+        db.session.add(num)
+
+        db.session.commit()
+
+        return redirect(url_for('phonebook'))
+    return render_template('submitnum.html', num = num)
+
+@app.route('/num_detail/<int:num_id>')
+@login_required
+def num_detail(num_id):
+    num = AvengerNum.query.get_or_404(num_id)
+    return render_template('num_detail.html', num = num)
+
+@app.route('/num_detail/update/<int:num_id>', methods = ['GET', 'POST'])
+@login_required
+def num_update(num_id):
+    nums = AvengerNum.query.get_or_404(num_id)
+    update_contact = PostNum()
+
+    if request.method == 'POST' and update_contact.validate():
+        name = update_contact.avenger_name.data
+        num = update_contact.phone_num.data
+        user_id = current_user.id
+        print(name, num, user_id)
+
+        nums.name = name
+        nums.num = num
+        nums.user_id = user_id
+
+        db.session.commit()
+        return redirect(url_for('phonebook.html', num_id = num.id))
+    return render_template('phonebook.html', update_num = update_num)
+
+@app.route('/num_detail/delete/<int:num_id>', methods=['POST'])
+@login_required
+def post_delete(num_id):
+    num = AvengerNum.query.get_or_404(num_id)
+    db.session.delete(num)
+    db.session.commit()
+    return redirect(url_for('phonebook'))
+
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = UserInfoForm()
@@ -31,58 +83,6 @@ def register():
         msg.html = ('<h1> Welcome to Call An Avenger! </h1>' '<p> This will be fun! </p>')
         mail.send(msg)
     return render_template('register.html', form = form)
-
-@app.route('/submitnum', methods=['GET','POST'])
-@login_required
-def submitnum():
-    num = PostNum()
-    if request.method == 'POST' and post.validate():
-        name = num.avenger_name.data
-        phone = num.phone_num.data
-        user_id = current_user.id
-        print('\n', name, phone)
-        num = AvengerNum(name, phone, user_id)
-
-        db.session.add(num)
-
-        db.session.commit()
-
-        return redirect(url_for('phonebook'))
-    return render_template('submitnum.html', num = num)
-
-@app.route('/submitnum/<int:num_id>')
-@login_required
-def num_detail(num_id):
-    num = AvengerNum.query.get_or_404(post_id)
-    return render_template('num_detail.html', num = num)
-
-@app.route('/submitnum/update/<int:num_id>', methods = ['GET', 'POST'])
-@login_required
-def num_update(num_id):
-    nums = AvengerNum.query.get_or_404(num_id)
-    update_contact = PostNum()
-
-    if request.method == 'POST' and update_contact.validate():
-        name = update_contact.avenger_name.data
-        num = update_contact.phone_num.data
-        user_id = current_user.id
-        print(name, num, user_id)
-
-        nums.name = name
-        nums.num = num
-        nums.user_id = user_id
-
-        db.session.commit()
-        return redirect(url_for('num_update', num_id = num.id))
-    return render_template('num_update.html', update_num = update_num)
-
-@app.route('/submitnum/delete/<int:num_id>', methods=['POST'])
-@login_required
-def post_delete(num_id):
-    num = AvengerNum.query.get_or_404(num_id)
-    db.session.delete(num)
-    db.session.commit()
-    return redirect(url_for('phonebook'))
 
 @app.route('/login',methods=['GET','POST'])
 def login():
